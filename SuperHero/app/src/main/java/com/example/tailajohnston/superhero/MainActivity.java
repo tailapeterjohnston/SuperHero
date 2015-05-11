a20cd2091215e44c59f0cb498682f796ef733f07
tailapeterjohnston <tailapeterjohnston@gmail.com>
2015-05-11 19:43:48 +1200

    Managed to get the phone to connect new interface, Main activity is the launcher It logs and tells me when the GPS button is pressed
    **UPDATE**
    When GPS Button it displays Latitude and Longitude on screen

--- SuperHero/app/src/main/java/com/example/tailajohnston/superhero/MainActivity.java
+++ SuperHero/app/src/main/java/com/example/tailajohnston/superhero/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.tailajohnston.superhero;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import android.widget.Button;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by tailajohnston on 9/05/15.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private static final String TAG = "tailasMessage";
 
@@ -18,15 +24,18 @@ public class MainActivity extends AppCompatActivity {
     Button gpsSMSButton;
     Button emergencyContactsButton;
 
+    TextView txtLat;
 
-//    public MainActivity(Bundle savedInstance) {
-//
-//        //set the layout for a new activity
-//        super.onCreate(savedInstance);
-//
-//
-//
-//    }
+
+    // ADDED FOR LOCATION
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         gpsSMSButton = (Button)findViewById(R.id.gpsSMSButton);
         emergencyContactsButton = (Button)findViewById(R.id.emergencyButton);
+        txtLat = (TextView)findViewById(R.id.latLongTextView);
+
+
+//        //start LatAndLongService
+//        Intent i = new Intent(this, FindLatLongService.class);
+//        startService(i);
+//        Log.i(TAG, "LatAndLongService has been executed in MainActivity ");
+//
+//        txtLat.setText(FindLatLongService.latnLong);
+//
 
         // set onClick listener for GPS Button
         gpsSMSButton.setOnClickListener(new Button.OnClickListener() {
@@ -44,15 +63,62 @@ public class MainActivity extends AppCompatActivity {
                 // Make Connection
                 // Find Location
                 // Get Emergency Contacts
-
+                getLocationChange();
+                gpsSMSButton.setTextColor(Color.RED);
                 Log.i(TAG, "gps Button pressed");
+
             }
         });
 
 
 
+        emergencyContactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // We go to contacts page.
+                emergencyContactsButton.setTextColor(Color.RED);
+                
+                Log.i(TAG, "Emergency Contacts button pressed");
+
+            }
+        });
+
+
         //Log
         Log.i(TAG, "onCreate");
+    }
+
+    public void getLocationChange() {
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        onLocationChanged(location);
+
+        Log.i(TAG, "getLocationChange");
+
+
+    }
+
+    public void onLocationChanged(Location location) {
+        txtLat = (TextView) findViewById(R.id.latLongTextView);
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        Log.i(TAG, "onLocationChanged");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
     }
 
     @Override
